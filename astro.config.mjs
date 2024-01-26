30b602e791309ba9e48c8d6729e764233ed729a9
Jan <57670746+Janoran@users.noreply.github.com>
2024-01-26 12:54:36 +0100

    Astro portfolio
    Yes

---
+++ astro.config.mjs
@@ -0,0 +1,10 @@
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+
+import sitemap from '@astrojs/sitemap';
+
+import { defineConfig } from 'astro/config'
+
+export default defineConfig({
+  site: 'https://github.com/Janoran/Janoran.github.io',
+});
